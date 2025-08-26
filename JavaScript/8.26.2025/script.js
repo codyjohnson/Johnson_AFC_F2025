751004819861a7269d446eb753abe7101b23f491
Cody Johnson <cody.johnson1@usmc.mil>
2025-08-26 12:03:06 -0500

    daily class work.

---
+++ JavaScript/8.26.2025/script.js
@@ -0,0 +1,19 @@
+// variables
+
+let num1 = "7";
+let num2 = 7;
+
+console.log(num1);
+console.log(num2);
+
+let unParsed = "234test";
+let parsed = parseInt(unParsed);
+console.log(parsed);
+
+let num3 = 0;
+
+console.log(" " == 0);
+console.log("" == 0);
+console.log("0.0" == 0);
+console.log("0.1" == 0.1);
+console.log(null == undefined);
